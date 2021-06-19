408b9614290d542776a9602c78354650fa913b08
Shintaro Fujiwara <shintaro.fujiwara@gmail.com>
2021-06-19 17:39:59 +0900

    Fix function.

--- COMMON/get_cve_info.sh
+++ COMMON/get_cve_info.sh
@@ -18,7 +18,7 @@
 ##  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 ##  02110-1301 USA
 ##
-## Version: v0.0.1
+## Version: v0.0.2
 ## Written by Shintaro Fujiwara
 #################################
 
@@ -38,16 +38,16 @@ fi
 }
 
 wget -O "${CVENUMBER_FILE}" https://nvd.nist.gov/vuln/detail/"${CVENUMBER}"
-VULNERABILITY_DATE=$(grep -Hrn "vuln-published-on" "${CVENUMBER_FILE}"| awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }')
-VULNERABILITY_DATE_YEAR=$(echo "${VULNERABILITY_DATE}" | awk -F"/" '{ print $3"/" }')
-VULNERABILITY_DATE_MONTH_DAY=$(echo "${VULNERABILITY_DATE}" | awk -F"/" '{ print $1"/"$2 }')
-VULNERABILITY_DATE=$(echo "${VULNERABILITY_DATE_YEAR}${VULNERABILITY_DATE_MONTH_DAY}")
+VULNERABILITY_DATE=$(grep -Hrn "vuln-published-on" "${CVENUMBER_FILE}"| awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }' | sed -e 's///g')
+VULNERABILITY_DATE_YEAR=$(echo "${VULNERABILITY_DATE}" | awk -F"/" '{ print $3"/" }' | sed -e 's///g')
+VULNERABILITY_DATE_MONTH_DAY=$(echo "${VULNERABILITY_DATE}" | awk -F"/" '{ print $1"/"$2 }' | sed -e 's///g')
+VULNERABILITY_DATE=$(echo "${VULNERABILITY_DATE_YEAR}${VULNERABILITY_DATE_MONTH_DAY}" | sed -e 's/^M//g')
 VULNERABILITY_DESCRIPTION=$(grep -Hrn "vuln-analysis-description\"" "${CVENUMBER_FILE}")
-VULNERABILITY_DESCRIPTION=$(echo "${VULNERABILITY_DESCRIPTION}" | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }')
-VECTOR_STRING2=$(grep -Hrn "tooltipCvss2NistMetrics\">(AV" "${CVENUMBER_FILE}" | sed -e 's/.*>(//' | sed -e 's/).*//')
-VECTOR_STRING3=$(grep -Hrn "CVSS\:3" "${CVENUMBER_FILE}" | sed -e 's/.*Metrics">//' | sed -e 's/<.*>//')
-ATTACK_VECTOR2=$(echo "${VECTOR_STRING2}" | awk -F":" '{ print $2 }' | awk -F"/" '{ print $1}')
-ATTACK_VECTOR3=$(echo "${VECTOR_STRING3}" | awk -F"/" '{ print $2 }' | awk -F":" '{ print $2}')
+VULNERABILITY_DESCRIPTION=$(echo "${VULNERABILITY_DESCRIPTION}" | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }' | sed -e 's///g')
+VECTOR_STRING2=$(grep -Hrn "tooltipCvss2NistMetrics\">(AV" "${CVENUMBER_FILE}" | sed -e 's/.*>(//' | sed -e 's/).*//' | sed -e 's///g')
+VECTOR_STRING3=$(grep -Hrn "CVSS\:3" "${CVENUMBER_FILE}" | sed -e 's/.*Metrics">//' | sed -e 's/<.*>//' | sed -e 's///g')
+ATTACK_VECTOR2=$(echo "${VECTOR_STRING2}" | awk -F":" '{ print $2 }' | awk -F"/" '{ print $1}' | sed -e 's///g')
+ATTACK_VECTOR3=$(echo "${VECTOR_STRING3}" | awk -F"/" '{ print $2 }' | awk -F":" '{ print $2}' | sed -e 's///g')
 get_access_vector "${ATTACK_VECTOR2}"
 ATTACK_VECTOR2="${AV}"
 get_access_vector "${ATTACK_VECTOR3}"
@@ -60,18 +60,18 @@ echo "VULNERABILITY_DATE:${VULNERABILITY_DATE}"
 echo "VULNERABILITY_DESCRIPTION:${VULNERABILITY_DESCRIPTION}"
 echo "VECTOR_STRING2:${VECTOR_STRING2}"
 echo "ATTACK_VECTOR2:${ATTACK_VECTOR2}"
-VALUE_VERSION2=$(echo "${VALUE_VERSION2}" | sed -e 's/.*warning">//' | sed -e 's/<.*>//')
-SCORE_VERSION2=$(echo "${VALUE_VERSION2}" | awk -F" " '{ print $1 }')
-SEVERITY_VERSION2=$(echo "${VALUE_VERSION2}" | awk -F" " '{ print $2 }')
+VALUE_VERSION2=$(echo "${VALUE_VERSION2}" | sed -e 's/.*warning">//' | sed -e 's/<.*>//' | sed -e 's///g')
+SCORE_VERSION2=$(echo "${VALUE_VERSION2}" | awk -F" " '{ print $1 }' | sed -e 's///g')
+SEVERITY_VERSION2=$(echo "${VALUE_VERSION2}" | awk -F" " '{ print $2 }' | sed -e 's///g')
 echo "SCORE_VERSION2:${SCORE_VERSION2}"
 echo "SEVERITY_VERSION2:${SEVERITY_VERSION2}"
 echo "VECTOR_STRING3:${VECTOR_STRING3}"
 echo "ATTACK_VECTOR3:${ATTACK_VECTOR3}"
 VALUE_VERSION3=$(echo "${VALUE_VERSION3}")
-SCORE_VERSION3=$(echo "${VALUE_VERSION3}" | awk -F"class=\"label" '{ print $2 }' | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }')
-SCORE_AND_SEVERITY_VERSION3=$(echo "${SCORE_VERSION3}" | xargs)
-SCORE_VERSION3=$(echo "${SCORE_AND_SEVERITY_VERSION3}" | awk -F " " '{ print $1 }')
-SEVERITY_VERSION3=$(echo "${SCORE_AND_SEVERITY_VERSION3}" | awk -F" " '{ print $2 }')
+SCORE_VERSION3=$(echo "${VALUE_VERSION3}" | awk -F"class=\"label" '{ print $2 }' | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }' | sed -e 's///g')
+SCORE_AND_SEVERITY_VERSION3=$(echo "${SCORE_VERSION3}" | sed -e 's///g' |xargs)
+SCORE_VERSION3=$(echo "${SCORE_AND_SEVERITY_VERSION3}" | awk -F " " '{ print $1 }' | sed -e 's///g')
+SEVERITY_VERSION3=$(echo "${SCORE_AND_SEVERITY_VERSION3}" | awk -F" " '{ print $2 }' | sed -e 's///g')
 echo "SCORE_VERSION3:${SCORE_VERSION3}"
 echo "SEVERITY_VERSION3:${SEVERITY_VERSION3}"
 unlink "${CVENUMBER_FILE}"
